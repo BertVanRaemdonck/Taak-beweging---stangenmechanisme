362de0a1913cf191e63a556e626d485b4f8bfdd2
BertVanRaemdonck <bert1.vanraemdonck@telenet.be>
2016-03-03 20:00:45 +0100

    Code assembly figuur
    Code ingevuld om ervoor te zorgen dat we een figuur van het gesloten mechanisme te zien krijgen. Deze code is grotendeels gekopieerd uit het animatiestuk, dus als er daar ergens een fout zit, hier waarschijnlijk ook.

--- kinematics_4bar.m
+++ kinematics_4bar.m
@@ -252,14 +252,33 @@ if fig_kin_4bar
     
     %plot assembly at a certain timestep 
     index = 1; %select 1st timestep
-    P = 0;
-    S = r1*exp(j*phi1);
-    Q = P + r2 * exp(j*phi2(index));
-    R = Q + r3 * exp(j*phi3(index));
+    sch_1_2 = 0;
+    sch_1_7 = (x7 + j*y7)*exp(j*phi1);
+    sch_1_4 = (x4 + j*y7)*exp(j*phi1);
+    
+    sch_2_12  = sch_1_2 + r2l*exp(j*phi2(index) + pi);
+    sch_2_3   = sch_1_2 + r2k*exp(j*phi2(index) - pi/2);
+    sch_3_4   = sch_2_3 + r3*exp(j*phi3(index) + pi);
+    sch_5_6   = sch_1_4 + x5(index)*exp(j*phi4(index)); % Het blokje dat over de roterende staaf glijdt
+    sch_6_7   = sch_1_7 + r7*exp(j*phi7(index) + pi);
+    sch_6_8   = sch_6_7 + r6*exp(j*phi6(index));
+    sch_8_9   = r2l + r12 + x9(index) + j*y9; % De bovenste van de twee zuigers
+    sch_8_10  = sch_6_8 + (r8l + r8k)*exp(j*phi8(index) + pi);
+    sch_10_11 = r2l + r12 + x11(index) - j*r11;
+    sch_11_12 = sch_2_12 + r12*exp(j*phi12(index));
+    
+    hoekpunt_4 = sch_1_4 + a*exp(j*phi4(index)); % Het hoekpunt van staaf 4
     
     figure
-    assembly=[P, Q, R, S];
-    plot(real(assembly),imag(assembly),'ro-')
+    
+    staaf2 = [sch_1_2 sch_2_3 sch_2_12 sch_1_2]; % De driehoekige staaf 2
+    loop1  = [sch_2_12 sch_11_12 sch_10_11 sch_8_10 sch_8_9 sch_6_8 sch_5_6 sch_6_7 sch_1_7]
+    loop2  = [sch_1_4 hoekpunt_4 sch_3_4 sch_2_3];
+    
+    plot(real(staaf2), imag(staaf2), 'ro-')
+    hold on;
+    plot(real(loop1), imag(loop1), 'ro-')
+    plot(real(loop2), imag(loop2), 'ro-')
     xlabel('[m]')
     ylabel('[m]')
     title('assembly')
