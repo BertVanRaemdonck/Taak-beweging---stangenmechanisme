33dc1bb87d2c012e4e012b2919b2d7e116d94aec
BenVandenBroeck <4d428b0a@opayq.com>
2016-04-28 22:01:09 +0200

    Omdat we het toch hebben, de betere berekening van de veer eigenschappen
    --> Ik zou gewoon verwijzen in het verslag naar de code, laten zien dat we er toch aan gewerkt hebben.

--- nokken.m
+++ nokken.m
@@ -907,3 +907,52 @@ axis([min_contact_x max_contact_x min_contact_y_adjusted max_contact_y_adjusted]
 
 hold off
 
+
+%% Extra calculation contact force multi rise
+
+
+    % Calculate forces
+spring_force = F_v0 + k*follower_motion;
+transient_force = k_follower*(output_motion - follower_motion);
+contact_force = (spring_force + transient_force) ./ cos(pi/180*alpha_multi);
+
+
+k_adjusted = k_double;              % Schatting van een betere k, wordt erna opnieuw berekend, maar zal niet veel verschillen
+
+    % Calculate the preload needed to maintain contact
+compensation = -min(F_v0 + transient_force(66:length(transient_force)) +k_adjusted*follower_motion(66:length(transient_force)));   % Aangepaste veer van single rise helpt
+F_v0_adjusted = F_v0;
+if compensation > 0
+    F_v0_adjusted = F_v0_adjusted + 5*ceil(compensation/5)      % Rounds up to next multiple of 5
+end   
+
+% Calculate the spring constant needed to maintain contact
+k_adjusted = max((-transient_force(1:66)-F_v0_adjusted)./ follower_motion(1:66))
+k_adjusted = 5*ceil(k_adjusted/5);          % rounds up to the next multiple of 5
+
+    % Calculate the new forces after adjustment of the spring constant and preload
+spring_force_adjusted = F_v0_adjusted + k_adjusted*follower_motion;
+contact_force_adjusted = (spring_force_adjusted + transient_force) ./ cos(pi/180*alpha_multi);
+
+
+% % Plot results
+min_contact_x = 361*(times_repeating-1);
+max_contact_x = 361*(times_repeating);
+min_contact_y = min(contact_force)*1.2;
+max_contact_y = max(contact_force)*1.2;
+
+
+figure('Name', 'Overgangsverschijnsel krachten multirise extra', 'NumberTitle', 'off');
+plot(contact_force)
+axis([min_contact_x max_contact_x min_contact_y max_contact_y])
+
+
+min_contact_y_adjusted = min(contact_force_adjusted)*1.2;
+max_contact_y_adjusted = max(contact_force_adjusted)*1.2;
+
+
+figure('Name', 'Overgangsverschijnsel krachten multirise adjusted spring extra', 'NumberTitle', 'off');
+plot(contact_force_adjusted)
+axis([min_contact_x max_contact_x min_contact_y_adjusted max_contact_y_adjusted])
+
+
